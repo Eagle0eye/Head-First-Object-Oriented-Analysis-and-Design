e044db1313676df980cc114e9074ea3447b93bff
Yousef Mohamed <yousef.mohamed.12@hotmail.com>
2024-05-18 23:03:37 +0300

    Phase 3-Test Application

--- chapter 2/DogDoorSimulator/src/main/java/org/DogDoorSimulator/Main.java
+++ chapter 2/DogDoorSimulator/src/main/java/org/DogDoorSimulator/Main.java
@@ -4,11 +4,21 @@ package org.DogDoorSimulator;public class Main {
         DogDoor door = new DogDoor();
         Remote remote = new Remote(door);
 
-        System.out.println("Fido barks to go outside...");
+        System.out.println();
         remote.pressButton();
-        System.out.println("\nFido has gone outside...");
+        System.out.println("Fido barks to go outside...");
         System.out.println("\nFido’s all done...");
-        System.out.println("\nFido’s back inside...");
 
+        try {
+            Thread.currentThread().sleep(10000);
+
+        }catch (InterruptedException e)
+        {
+            System.out.println("...but he's stuck outside!");
+            System.out.printf("\nFido starts breaking....");
+            System.out.println("...so Gina grabs the remote control");
+            remote.pressButton();
+            System.out.println("\nFido’s back inside...");
+        }
     }
 }
